246cec2c5d442c195519ddab8e20a75824c7c82c
Adam Zakaria <adam.p.zakaria@gmail.com>
2023-08-20 21:50:59 -0400

    Add MyComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.scss'
+import MyComponent from './MyComponent'
 
 function App() {
   const [count, setCount] = useState(0)
+  /*
+  read from 4 files, put them in 4 flex col divs
+  */
 
   return (
     <>
-      <div className='bg-[#50d71e]'>Hello world</div>
+      <MyComponent></MyComponent>
     </>
   )
 }
---
+++ src/MyComponent.jsx
@@ -0,0 +1,12 @@
+function MyComponent() {
+  return (
+  <div className="bg-blue-500 p-10">
+    <div className="bg-red-500">
+        Cash me outside
+    </div>
+  </div>
+
+  );
+}
+
+export default MyComponent;
